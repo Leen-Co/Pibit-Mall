f47dbe4c3558540805698e6f639646ac09d0e5ae
Leen-Co <125503674+Leen-Co@users.noreply.github.com>
2023-03-18 10:48:19 +0100

    Delete src directory

--- src/service/helper.js
+++
@@ -1,8 +0,0 @@
-// Localhost URI
-export const MAIN_URI =
-  window.location.hostname === "localhost"
-    ? "http://localhost:4000"
-    : "https://themenhood.onrender.com";
-
-// Production URI
-export const FRONT_MAIN_URI = "https://themenhood.netlify.com";
